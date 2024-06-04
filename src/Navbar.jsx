3ed9dc8b8ba165a4f2353c8539e1ab55061b0afd
Usman-837 <usmantufail837@gmail.com>
2024-06-04 12:46:06 +0500

    Add Logo in Navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,8 +5,13 @@ const Navbar = () => {
     return (
         <>
         {/* Navbar  */}
-
         <nav className="flex justify-evenly items-center bg-[#0F1111] text-white text-sm">
+            
+            {/* logo */}
+            <a href="#" className="border border-transparent p-1 hover:border-white">
+                <img src= {logo} alt="" className="w-24 h-10 mt-2"/>
+            </a>
+        
         </nav>
         </>
     );
