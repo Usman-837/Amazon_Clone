154d247ac6bb29f10be681c072051656f079ff37
Usman-837 <usmantufail837@gmail.com>
2024-06-04 12:41:12 +0500

    Initialize Amazon Clone Navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+import logo from "./assets/amazon_logo.png";
+import flag from "./assets/USFlag.png";
+const Navbar = () => {
+    
+    return (
+        <>
+        {/* Navbar  */}
+
+        <nav className="flex justify-evenly items-center bg-[#0F1111] text-white text-sm">
+        </nav>
+        </>
+    );
+}
+
+export default Navbar;
